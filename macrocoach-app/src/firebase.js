c28d0432bd1931360571a7db0d984b1666db99e3
M4ximili4nSll <maximiliansell93@gmail.com>
2025-10-19 19:40:48 +0200

    .env

--- macrocoach-app/src/firebase.js
+++ macrocoach-app/src/firebase.js
@@ -12,12 +12,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAx5EaagxsSRrVT7fJtRzthViXjR8Q7LS0",
-    authDomain: "nutrition-coach-f9ce8.firebaseapp.com",
-    projectId: "nutrition-coach-f9ce8",
-    storageBucket: "nutrition-coach-f9ce8.appspot.com",
-    messagingSenderId: "434213023999",
-    appId: "1:434213023999:web:9130bb8258112ed08b1cca"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
